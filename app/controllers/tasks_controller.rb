e900e318e84a40ddc449fb191b68d3e0c791afca
ichiro.suzuki <kakedashiitt@gmail.com>
2021-09-17 09:46:01 +0000

    post5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,18 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
+
   
   def index
     if logged_in?
-      @tasks = Task.all  # form_with 用
-      @pagy, @tasks = pagy(Task.order(id: :desc), items: 3)
+    @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
     end
   end
   
   def show
-    
+    @task = Task.find(params[:id])
   end
   
   def new
@@ -46,7 +47,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'メッセージは正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -62,7 +63,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-    end
       redirect_to root_url
+    end
   end
 end
